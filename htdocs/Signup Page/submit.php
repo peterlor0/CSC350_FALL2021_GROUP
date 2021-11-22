82eb01994b92923fd22b7fd658fa977eaa2cf311
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-11-22 13:42:44 -0500

    3

--- htdocs/Signup Page/submit.php
+++ htdocs/Signup Page/submit.php
@@ -1,23 +1,35 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    
-    // Create connection
-    $conn = mysqli_connect($servername,$username,$password);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-    
-    $sql = "INSERT INTO mgr.userdata (Username, Password)
-    VALUES ({$_POST['username']}, {$_POST['password']})";
-    
-    if ($conn->query($sql) === TRUE) {
-      echo "user created";
-    } else {
-      echo "Could not create user";
-    }
-    
-    $conn->close();
-?>
+<html>
+
+<head>
+  <link rel="stylesheet" href="./../shared.css">
+</head>
+
+<body>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO mgr.userdata (Username, Password)
+    VALUES ('{$_POST['username']}', '{$_POST['password']}')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "<p>user created</p><br>";
+    echo "<a href='../main.html'>Continue to login</a>";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+  ?>
+</body>
+
+</html>
