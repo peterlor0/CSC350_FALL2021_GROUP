d19341816ea5a1da119420347c18d6c26697d0d7
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-11-22 15:11:00 -0500

    1

---
+++ htdocs/main page/main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="./../shared.css">
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM mgr.userdata WHERE Username='{$_POST['username']}'";
+    $ret = $conn->query($sql);
+
+    if ($ret) {
+        $row = mysqli_fetch_row($ret);
+
+        if ($row) {
+            if ($row[1] == $_POST['password']) {
+                echo "welcome back";
+            } else {
+                echo "password error";
+            }
+        } else {
+            echo "user not found";
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+    ?>
+</body>
+
+</html>
