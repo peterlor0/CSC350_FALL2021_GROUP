9cc85981d4b18f875ad7826bfdf261f42687ebe2
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-11-29 14:53:41 -0500

    Update main.php

--- htdocs/main page/main.php
+++ htdocs/main page/main.php
@@ -13,7 +13,12 @@
     <?php
     session_start();
     if (isset($_SESSION['username'])) {
-        echo "welcome back! " . $_SESSION['username'];
+        $conn = startSQLConnect();
+        $sql = "SELECT * FROM mgr.userdata WHERE Username='{$_SESSION['username']}'";
+        $ret = $conn->query($sql);
+        $row = mysqli_fetch_row($ret);
+        echo "welcome back! " . $_SESSION['username'] . "<br>";
+        echo "Room Number: " . $row[2];
     } else {
         echo "error";
     }
