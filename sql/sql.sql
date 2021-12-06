16592f459be2d20e3d75015329bcdef7eb44f333
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-12-05 21:21:01 -0500

    Update sql.sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -18,6 +18,29 @@ USE `mgr`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `schedule` (
+  `Date` datetime NOT NULL,
+  `Username` varchar(45) NOT NULL,
+  UNIQUE KEY `Date_UNIQUE` (`Date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule`
+--
+
+LOCK TABLES `schedule` WRITE;
+/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
+/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `userdata`
 --
 
@@ -39,7 +62,7 @@ CREATE TABLE `userdata` (
 
 LOCK TABLES `userdata` WRITE;
 /*!40000 ALTER TABLE `userdata` DISABLE KEYS */;
-INSERT INTO `userdata` VALUES ('aa','aa','666'),('aaa','12345678','111');
+INSERT INTO `userdata` VALUES ('a','a','1'),('aa','aa','666'),('aaa','12345678','111');
 /*!40000 ALTER TABLE `userdata` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-29 16:26:21
+-- Dump completed on 2021-12-05 21:20:32
