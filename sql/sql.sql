d1f1b95b10f8e9d916333c63385d6b40a19f454c
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-12-10 01:26:45 -0500

    Update sql.sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -27,7 +27,9 @@ DROP TABLE IF EXISTS `schedule`;
 CREATE TABLE `schedule` (
   `Date` datetime NOT NULL,
   `Username` varchar(45) NOT NULL,
-  UNIQUE KEY `Date_UNIQUE` (`Date`)
+  UNIQUE KEY `Date_UNIQUE` (`Date`),
+  KEY `fk_user_idx` (`Username`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`Username`) REFERENCES `userdata` (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-06 18:41:05
+-- Dump completed on 2021-12-10  1:26:20
