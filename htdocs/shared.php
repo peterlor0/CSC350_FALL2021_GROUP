e6860553ce4122333200b432fe6a4c3375c7f5f4
hahah1234567 <94410907+hahah1234567@users.noreply.github.com>
2021-12-11 20:50:46 -0500

    1

--- htdocs/shared.php
+++ htdocs/shared.php
@@ -96,7 +96,7 @@ function getDateTimeRangeOfNextWeekSchedule()
 function isAvailableForNextWeekSchedule()
 {
     //"this week + 6 day" is Sunday
-    if (time() >= strtotime("this week")) {
+    if (time() >= strtotime("this week + 6 day")) {
         return true;
     } else {
         return false;
